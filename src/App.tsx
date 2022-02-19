26e165bc5738ed4e1991f63099c68df35b3a48f6
Chun-gu <chungu.yi92@gmail.com>
2022-02-19 23:09:15 +0900

    ✨ DragDropContext, Droppable, Draggable 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 function App() {
-  return <div></div>;
+  const onDragEnd = () => {};
+
+  return (
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="list">
+          {() => (
+            <ul>
+              <Draggable draggableId="first-item" index={0}>
+                {() => <li>first</li>}
+              </Draggable>
+              <Draggable draggableId="second-item" index={1}>
+                {() => <li>second</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </>
+  );
 }
 
 export default App;
