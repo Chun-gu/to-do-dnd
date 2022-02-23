a39ecf7f1d660ea50d49af44f9d70716cd2c23ef
Chun-gu <chungu.yi92@gmail.com>
2022-02-23 23:54:38 +0900

    ✨ recoil root 적용

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import App from "./App";
 import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
