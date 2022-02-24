8da4d7561ea540046b2dd8b4f4b8d255e4f98390
Chun-gu <chungu.yi92@gmail.com>
2022-02-24 22:13:51 +0900

    ✨ 각 Board의 title 추가

--- src/components/DroppableBoard/index.tsx
+++ src/components/DroppableBoard/index.tsx
@@ -1,5 +1,5 @@
 import { Droppable } from "react-beautiful-dnd";
-import { Boards, Board } from "./styled";
+import { Boards, Board, Title } from "./styled";
 import { DraggableCard } from "..";
 import { useRecoilValue } from "recoil";
 import { toDoState } from "../../atoms";
@@ -8,18 +8,22 @@ interface IBoardProps {
   toDos: string[];
   boardId: string;
 }
+
 function Index({ toDos, boardId }: IBoardProps) {
   return (
-    <Droppable droppableId={boardId}>
-      {(provided) => (
-        <Board ref={provided.innerRef} {...provided.droppableProps}>
-          {toDos.map((toDo, index) => (
-            <DraggableCard key={toDo} index={index} toDo={toDo} />
-          ))}
-          {provided.placeholder}
-        </Board>
-      )}
-    </Droppable>
+    <>
+      <Title>{boardId}</Title>
+      <Droppable droppableId={boardId}>
+        {(provided) => (
+          <Board ref={provided.innerRef} {...provided.droppableProps}>
+            {toDos.map((toDo, index) => (
+              <DraggableCard key={toDo} index={index} toDo={toDo} />
+            ))}
+            {provided.placeholder}
+          </Board>
+        )}
+      </Droppable>
+    </>
   );
 }
 
