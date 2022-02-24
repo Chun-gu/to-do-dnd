3e1bd8207ce9c2fa8cad440435215d7e37e650bf
Chun-gu <chungu.yi92@gmail.com>
2022-02-24 22:20:54 +0900

    ✨ Droppable 컴포넌트 분리

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,11 +1,25 @@
 import { useRecoilState } from "recoil";
 import { toDoState } from "../../atoms";
+import DroppableBoard from "../DroppableBoard";
+import { Boards } from "../DroppableBoard/styled";
 import { Wrapper } from "./styled";
 
 const Index = () => {
   const [toDos, setToDos] = useRecoilState(toDoState);
 
-  return <Wrapper>index</Wrapper>;
+  return (
+    <Wrapper>
+      <Boards>
+        {Object.keys(toDos).map((boardId) => (
+          <DroppableBoard
+            key={boardId}
+            boardId={boardId}
+            toDos={toDos[boardId]}
+          />
+        ))}
+      </Boards>
+    </Wrapper>
+  );
 };
 
 export default Index;
