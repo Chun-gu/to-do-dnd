17af2af0b04886c6ed9898f01b9bb02eafbd2ff4
Chun-gu <chungu.yi92@gmail.com>
2022-04-14 23:57:25 +0900

    💄 색상 hsl로 바꾸고 팔레트 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "#3F8CF2",
-  boardColor: "#DADFE9",
-  cardColor: "white",
+  bgColor: "hsl(214, 80%, 60%)",
+  boardColor: "	hsl(220, 25%, 80%)",
+  cardColor: "#ffffff",
 };
